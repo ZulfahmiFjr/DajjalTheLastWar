1907b91ec3a66ef59787cb019478c827471b9a35
Zulfahmi Fajri <mccraft874@gmail.com>
2025-12-24 22:05:59 +0700

    edit backend

--- backend/src/main/java/com/finpro7/server/repository/UserRepository.java
+++ backend/src/main/java/com/finpro7/server/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.finpro7.server.repository;
 
 import com.finpro7.server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
